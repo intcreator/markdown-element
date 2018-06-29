5d50b1d61c9ecde829d411633cf20f8f950a17ec
Brandon der Blätter <intcreator@gmail.com>
2018-06-29 13:27:54 -0600

    Set up package.json, element, .gitignore, etc.

---
+++ markdown-element.js
@@ -0,0 +1,71 @@
+import { PolymerElement, html } from './node_modules/@polymer/polymer/polymer-element.js';
+import './node_modules/commonmark/dist/commonmark.js';
+import './node_modules/prismjs/prism.js';
+
+class MarkdownElement extends PolymerElement {
+    
+    static get template() {
+        return html`
+            <style>
+
+                :host {
+                    display: block;
+                }
+
+            </style>
+
+            <slot name="markdown-html">
+                <div id="content"></div>
+            </slot>
+        `
+    }
+
+    static get properties() {
+        return {
+            markdown: {
+                type: String,
+                observer(newValue, oldValue) {
+                    // parse and render Markdown
+                    const reader = new commonmark.Parser();
+                    const writer = new commonmark.HtmlRenderer();
+                    const html = writer.render(reader.parse(newValue));
+                    // set HTML of slot (if given) or div
+                    const target = this.querySelector('[slot="markdown-html"]') || this.shadowRoot.querySelector('#content')
+                    target.innerHTML = html;
+                    // highlight all code blocks under the target
+                    const things = Prism.highlightAllUnder(target, false);
+                }
+            },
+            src: {
+                type: String,
+                observer(newValue, oldValue) {
+                    this.fetchMarkdown(newValue);
+                }
+            }
+        };
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        // return if markdown or a reference was passed in through a data binding
+        if(this.markdown || this.src) return;
+        // otherwise look for a script tag
+        const markdownScript = this.querySelector('script[type="text/markdown"]')
+        // if there's no script tag, return
+        if(!markdownScript) return;
+    }
+
+    // fetch the markdown and set it locally
+    async fetchMarkdown(src) {
+        await fetch(src)
+            .then(async response => {
+                this.markdown = await response.text();
+            })
+            .catch(e => {
+                console.error(e)
+            })
+    }
+
+}
+
+customElements.define('markdown-element', MarkdownElement);
