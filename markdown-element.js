0d019e5d144a74329d564ddf5245c9aca4ba2984
Brandon der Blätter <intcreator@gmail.com>
2018-07-02 23:14:12 -0600

    Fix rendering race condition

--- markdown-element.js
+++ markdown-element.js
@@ -1,11 +1,12 @@
 import { LitElement, html } from '@polymer/lit-element/lit-element.js';
 import { unsafeHTML } from 'lit-html/lib/unsafe-html.js';
+import { until } from 'lit-html/lib/until.js';
 import 'commonmark/dist/commonmark.js';
 import 'prismjs/prism.js';
 
 class MarkdownElement extends LitElement {
     
-    _render({ markdown, src }) {
+    _render({ markdown, src }) {        
         return html`
             <style>
 
@@ -17,11 +18,8 @@ class MarkdownElement extends LitElement {
 
             </style>
             <h1>hey</h1>
-            ${ this.renderMarkdown(markdown, src) }
-            <!-- <slot name="markdown-html">
-                <div id="content">No Markdown specified or failed to load</div>
-            </slot> -->
-        `
+            ${ until(this.renderMarkdown(markdown, src), 'Loading...') }
+        `;
     }
 
     static get properties() {
@@ -45,24 +43,17 @@ class MarkdownElement extends LitElement {
         this.markdown = markdownScript.text.trim();
     }
 
-    _didRender(props) {
-        // if(Object.keys(props).length === 0 && props.constructor === Object) return;
-        console.log(props, this.shadowRoot.querySelector('p'))
-        // this.shadowRoot.querySelector('code').style = 'background-color: red;';
-        // Prism.highlightAllUnder(this.shadowRoot.querySelector('code'), false);
+    async _didRender() {
+        // await a dummy renderMarkdown so shadowRoot is accessible
+        await this.renderMarkdown(this.markdown, this.src)
+        Prism.highlightAllUnder(this.shadowRoot, false);
     }
 
     // fetch the markdown and set it locally
     async fetchMarkdown(src) {
-        let text;
-        await fetch(src)
-            .then(async response => {
-                text = await response.text()
-            })
-            .catch(e => {
-                return 'Failed to read Markdown source.'
-            })
-        return text;
+        return await fetch(src)
+            .then(async response => await response.text())
+            .catch(e => 'Failed to read Markdown source.')
     }
 
     async renderMarkdown(markdown, src) {
